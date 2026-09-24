74fc924e2025b09a1074c612c201d0bb026206b1
ccmatrix501-spec <ccmatrix501@gmail.com>
2026-09-24 14:57:41 +1000

    Bump Discord Activity shell to 1.6.5

--- src/discordActivity.ts
+++ src/discordActivity.ts
@@ -1,6 +1,6 @@
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
-const ACTIVITY_BUILD = "1.6.4-vite-native";
+const ACTIVITY_BUILD = "1.6.5-current-web-sync";
 const API_BASE = "/api";
 const REQUIRED_GUILD_ID = "1256977709884641382";
 
